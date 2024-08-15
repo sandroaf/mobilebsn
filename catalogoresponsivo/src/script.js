6f6be30a1d7113235269bddbb7dc184f204dbe2c
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2024-08-15 10:51:58 -0300

    Ajustes Menu responsivo

--- catalogoresponsivo/src/script.js
+++ catalogoresponsivo/src/script.js
@@ -1,18 +1,24 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
     const menuHam = document.querySelector('.ham-menu');
-    console.log(menuHam);
-    const menu = document.querySelector('nav#menu').tex;
-    var tmenu = menuHam.textContent;
+    const menu = document.querySelector('nav#menu');
+    var chammenuclose = "true";
+    var thammenu = menuHam.textContent;
     
     menuHam.addEventListener('click', function() {
-        if (menuHam.textContent == "&equiv;") {
-            menu.style.display="inline-block";
-            tmenu = menuHam.textContent;
-            menuHam.textContent = "X";
+
+        if (chammenuclose) {
+            menu.classList.toggle("active");
+            console.log(menu.classList);
+            thammenu = menuHam.textContent;
+            menuHam.textContent = "x";
+            chammenuclose = false;
         } else {
-            menu.style.display="none";
-            menuHam.textContent = tmenu;
+            menu.classList.toggle("active");
+            console.log(menu.classList);
+            menuHam.textContent = thammenu;
+            thammenu = menuHam.textContent;
+            chammenuclose = true;
         }
     });
 });
